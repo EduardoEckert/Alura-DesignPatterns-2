2072a0b44fe58bd9497db2cd800d43e584133a32
Eduardo Eckert Sperfeld <eduardo.sperfeld@ambevtech.com.br>
2022-12-19 14:16:24 -0300

    Criar Atividade09 - Façade / Singleton

---
+++ Alura-DesignPatterns-2/Atividade09/EmpresaFacadeSingleton.cs
@@ -0,0 +1,8 @@
+﻿namespace Alura_DesignPatterns_2.Atividade09
+{
+    public class EmpresaFacadeSingleton
+    {
+        private static EmpresaFacade facade = new EmpresaFacade();
+        public EmpresaFacade Instancia { get { return facade; } }
+    }
+}
